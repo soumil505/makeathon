282d0ca81e2f25129bc04a9298808f4e7c4240cc
soumil505 <shahsoumil.santosh2016@vitstudent.ac.in>
2018-10-05 11:50:47 +0530

    crop view final

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var mongoose=require('mongoose');
+
+
+mongoose.connect('mongodb://soumil:soumil32@ds223253.mlab.com:23253/makeathon').then(() => {
+    console.log('Database connection successful')
+})
+    .catch(err => {
+        console.error(err)
+    });
+var userSchema = new mongoose.Schema({
+    "name":String,
+    "humidity":String,
+    "temperature":String
+});
+
+var userModel=mongoose.model('user',userSchema);
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,7 +25,19 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/cropView',function (req,res,next) {
-    res.render("cropView.ejs");
+
+    userModel.find({},(err,data)=>{
+        var hums=[];
+        var temps=[];
+        data.forEach((point)=>{
+            var humidity=point.humidity;
+            var temperature=point.temperature;
+            hums.push(humidity);
+            temps.push(temperature);
+        });
+        res.render("cropView.ejs",{h:hums,t:temps});
+    });
+
 });
 
 router.get('/analysisView',function (req,res,next) {
